3841ca037e47f07d2a505ab3b2a90e92308cd53b
Лев Халяпин <lev.halyapin@yandex.ru>
2021-10-04 22:54:03 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
     return HttpResponse('ГЛАВНАЯ страница')
 
-def group_posts(request):
+def group_posts(request, slug):
     return HttpResponse('Постики))')
